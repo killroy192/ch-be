ce6387aee69a96b406d743bc1249f67f4ac8710d
Dzmitry Lahunouski <dzmitry_lahunouski@epam.com>
2024-07-18 11:31:51 +0100

    fixes

--- packages/backend/prisma/migrations/20240718101051_init/migration.sql
+++ packages/backend/prisma/migrations/20240718101051_init/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "Post";
-
 -- CreateTable
 CREATE TABLE "Student" (
     "id" TEXT NOT NULL,
